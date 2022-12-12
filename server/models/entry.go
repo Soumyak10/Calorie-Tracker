bb3cdbe59087886cef8f18cdd73d57f58d21bed8
Siddharth Sahu <siddharth201820@gmail.com>
2022-12-12 23:58:23 +0530

    model changes

--- server/models/entry.go
+++ server/models/entry.go
@@ -1,10 +1,14 @@
 package models
 
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Entry struct {
-	// ID       primitive.ObjectID `bson:"id"`
-	Dish     *string  `json:"dish"`
-	Fats     *float64 `json:"fats"`
-	Calories *float64 `json:"calories"`
-	Carbs    *float64 `json:"carbs"`
-	Proteins *float64 `json:"proteins"`
+	ID       primitive.ObjectID `bson:"id"`
+	Dish     *string            `json:"dish"`
+	Fats     *float64           `json:"fats"`
+	Calories *float64           `json:"calories"`
+	Carbs    *float64           `json:"carbs"`
+	Proteins *float64           `json:"proteins"`
 }
